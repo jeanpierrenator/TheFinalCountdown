58dd941d04ea1ced60d4595385a515a82f3f1225
Guillaume Ropion <58080970+jeanpierrenator@users.noreply.github.com>
2022-09-27 20:19:19 +0200

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -7,8 +7,11 @@
 		//array of custom settings
 		var settings = { 
 			'date': null,
-			'format': null
+			'format': null,
+			'date2': null,
+			'format2': null
 		};
+
 
 		//append the settings array to options
 		if(options) {
@@ -21,10 +24,6 @@
 			eventDate = Date.parse(settings['date']) / 1000;
 			currentDate = Math.floor($.now() / 1000);
 			
-			if(eventDate <= currentDate) {
-				callback.call(this);
-				clearInterval(interval);
-			}
 			
 			seconds = eventDate - currentDate;
 			
@@ -68,7 +67,53 @@
 				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
 				clearInterval(interval); 
 			}
+			// compteur 2
+			eventDate = Date.parse(settings['date2']) / 1000;
+			currentDate = Math.floor($.now() / 1000);
+			if(eventDate <= currentDate) {
+				callback.call(this);
+				clearInterval(interval);
+			}
+			
+			seconds = eventDate - currentDate;
+			
+			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
+			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
+			
+			hours = Math.floor(seconds / (60 * 60));
+			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
+			
+			minutes = Math.floor(seconds / 60);
+			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
+
+			weeks = Math.floor(days / 7);
+
+			days = days % 7;
+			
+			
+			//logic for the two_digits ON setting
+			if(settings['format2'] == "on") {
+				weeks = (String(weeks).length >= 2) ? weeks : "0" + weeks;
+				days = (String(days).length >= 2) ? days : "0" + days;
+				hours = (String(hours).length >= 2) ? hours : "0" + hours;
+				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
+				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
+			}
+			
+			//update the countdown's html values.
+			if(!isNaN(eventDate)) {
+				thisEl.find(".weeks2").text(weeks);
+				thisEl.find(".days2").text(days);
+				thisEl.find(".hours2").text(hours);
+				thisEl.find(".minutes2").text(minutes);
+				thisEl.find(".seconds2").text(seconds);
+			} else { 
+				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
+				clearInterval(interval); 
+			}
 		}
+
+		
 		
 		//run the function
 		countdown_proc();
